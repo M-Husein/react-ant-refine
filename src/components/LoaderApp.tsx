3f1be095dfbbeadd2220bc79de99e2ff0afbf33e
M-Husein <m.husein27@gmail.com>
2024-05-23 10:50:50 +0700

    feat: change loader app hidden using class to hidden attribute

--- src/components/LoaderApp.tsx
+++ src/components/LoaderApp.tsx
@@ -3,11 +3,16 @@ import { useEffect } from 'react';
 export const LoaderApp: React.FC = () => {
   useEffect(() => {
     const loader = document.getElementById('loaderApp');
+
     // Show loading
-    loader?.classList.remove('hidden');
+    // loader?.classList.remove('hidden');
+
+    if(loader) loader.hidden = false;
     
     return () => { // Hide loading
-      loader?.classList.add('hidden');
+      // loader?.classList.add('hidden');
+
+      if(loader) loader.hidden = true;
     }
   }, []);
 
